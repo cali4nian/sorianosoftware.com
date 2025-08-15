922ecceb056b4d7739b8da72daf3c8e52307c861
Daniel Soriano <danielsoriano559@gmail.com>
2025-08-14 19:29:33 -0700

    logo.ico fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,9 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('images/logo.ico') }}" type="image/x-icon">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="text-white bg-gray-900">
